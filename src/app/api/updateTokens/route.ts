99c0115374c14a3be30f7da5871f02801b31a0b7
sinchangho <vkdghckdh@gmail.com>
2024-10-19 20:35:18 +0900

    vercel 스케줄러 설정

--- src/app/api/updateTokens/route.ts
+++ src/app/api/updateTokens/route.ts
@@ -21,8 +21,8 @@ const calculatePrice = (inputTokens: number, outputTokens: number) => {
 export async function GET() {
   try {
     // 1. Supabase에서 어제 날짜 데이터를 가져옴
-    // const today = new Date().toISOString().split("T")[0]; // YYYY-MM-DD 포맷으로 오늘 날짜 생성
-    const today = "2024-10-14";
+    const today = new Date().toISOString().split("T")[0]; // YYYY-MM-DD 포맷으로 오늘 날짜 생성
+    // const today = "2024-10-14";
     const { data, error } = await supabase
       .from("logs")
       .select("*")
