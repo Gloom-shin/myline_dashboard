4f522660e86a54755717c6c9024843f374271bd1
sinchangho <vkdghckdh@gmail.com>
2024-10-24 14:14:44 +0900

    하루 스케줄러 시간 땡기기

--- src/app/api/updateTokens/route.ts
+++ src/app/api/updateTokens/route.ts
@@ -23,8 +23,8 @@ const calculatePrice = (inputTokens: number, outputTokens: number) => {
 export async function GET() {
   try {
     // 1. Supabase에서 어제 날짜 데이터를 가져옴
-    const today = getKoreanDate(); // YYYY-MM-DD 포맷으로 오늘 날짜 생성
-    // const today = "2024-10-22";
+    // const today = getKoreanDate(); // YYYY-MM-DD 포맷으로 오늘 날짜 생성
+    const today = "2024-10-23";
     const { data, error } = await supabase
       .from("logs")
       .select("*")
